f235d7ca425b1f5fb6945ae1af2109447b7c8c3e
Vladimir Strugov <Strugov2001@yandex.ru>
2023-12-28 16:03:47 +0300

    add the mobile version of the main page

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -5,17 +5,17 @@ export default function AboutProject () {
     <section className='aboutProject' id='aboutProject'>
       <h2 className='main__subtitle'>О проекте</h2>
       <div className='aboutProject__textBox'>
-        <p className='aboutProject__text-title'>
+        <p className='aboutProject__text-title grid-title-first'>
           Дипломный проект включал 5 этапов
         </p>
-        <p className='aboutProject__text-title'>
+        <p className='aboutProject__text-title grid-title-second'>
           На выполнение диплома ушло 5 недель
         </p>
-        <p className='aboutProject__text-info'>
+        <p className='aboutProject__text-info grid-text-first'>
           Составление плана, работу над бэкендом, вёрстку, добавление
           функциональности и финальные доработки.
         </p>
-        <p className='aboutProject__text-info'>
+        <p className='aboutProject__text-info grid-text-second'>
           У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
           соблюдать, чтобы успешно защититься.
         </p>
